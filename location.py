f244366519d260f1de0c213d394401cc6376b774
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-25 15:33:39 +0900

    Update location.py

--- location.py
+++ location.py
@@ -14,26 +14,27 @@ location = st.selectbox("지역을 선택하세요:",("전국", "서울특별시
 
 family_name = st.text_input("성씨를 입력하세요:")
 
-st.write(location,"의 상위 5개 성씨")
+#st.write(location,"의 상위 5개 성씨")
 
-if st.button("검색"):
-    if location and family_name:
-        # Filter data based on the selected location and entered last name
-        filtered_data = data[(data['region'] == location) & (data['surname'] == family_name)]
-        
-        if not filtered_data.empty:
-            population_count = filtered_data.iloc[0]['population']
-            fullname_c = filtered_data.iloc[0]['fullname']
-            st.write(f"In {location}, there are {population_count} people with the last name {fullname_c}.")
+for index in data.iterrows():
+    if st.button("검색"):
+        if location and family_name:
+            # Filter data based on the selected location and entered last name
+            filtered_data = data[(data['region'] == location) & (data['surname'] == family_name)]
+            
+            if not filtered_data.empty:
+                population_count = filtered_data.iloc[0]['population']
+                fullname_c = filtered_data.iloc[0]['fullname']
+                st.write(f"In {location}, there are {population_count} people with the last name {fullname_c}.")
+            else:
+                st.write(f"No data found for the last name {family_name} in {location}.")
         else:
-            st.write(f"No data found for the last name {family_name} in {location}.")
+            st.write("Please select a region and enter a last name.")
+    
+    #    st.write(location, "Top 5 surnames")
+    
+    # Display top 5 surnames in the selected regionif location != "nationwide":
+    #    top_surnames = data[data['region'] == location].nlargest(5, 'population')[['surname', 'population']]
+    #    st.table(top_surnames)
     else:
-        st.write("Please select a region and enter a last name.")
-
-#    st.write(location, "Top 5 surnames")
-
-# Display top 5 surnames in the selected regionif location != "nationwide":
-#    top_surnames = data[data['region'] == location].nlargest(5, 'population')[['surname', 'population']]
-#    st.table(top_surnames)
-else:
-    st.write("Please select a specific region to see the top 5 surnames.")
+        st.write("Please select a specific region to see the top 5 surnames.")
