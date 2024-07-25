d094f76fcb1a1fc6ce8b7c3047edc020c220e7f0
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-25 13:17:59 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ st.write('Hello, *World!* :sunglasses:')
 st.title('간단한 인사말 생성기')
 
 st.write('이름을 입력하세요:')
-title = st.text_input(name_none, placeholder="한글이름")
+title = st.text_input("name_none", placeholder="한글이름")
 
 st.write('좋아하는 음식을 선택하세요:')
