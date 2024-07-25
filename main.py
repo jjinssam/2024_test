c3579bbaa6ede07eb24cc8f76ea9b12ff15d0dae
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-25 13:46:29 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.write('Hello, *World!* :sunglasses:')
 st.title('당신의 음식 취향')
 
-name = st.text_input("이름을 입력하세요:", value=st.session_state.name, placeholder="한글이름")
+name = st.text_input("이름을 입력하세요:",  placeholder="한글이름")
 food = st.selectbox("좋아하는 음식을 선택하세요:",("","망고빙수", "우유빙수", "복숭아빙수", "팥빙수"))
 
 if st.button("선택 후 눌러주세요", type="primary"):
