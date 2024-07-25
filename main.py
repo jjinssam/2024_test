7814d2162fe879c6a540752697ce8ebf59818b4e
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-25 13:21:53 +0900

    Update main.py

--- main.py
+++ main.py
@@ -4,5 +4,8 @@ st.write('Hello, *World!* :sunglasses:')
 st.title('간단한 인사말 생성기')
 
 title = st.text_input("이름을 입력하세요:", placeholder="한글이름")
+option = st.selectbox(
+    "좋아하는 음식을 선택하세요:",
+    ("망고빙수", "우유빙수", "복숭아빙수", "팥빙수"))
 
-st.write('좋아하는 음식을 선택하세요:')
+st.write(title,"씨가 좋아하는 음식은",option,"입니다.")
