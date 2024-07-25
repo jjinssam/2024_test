454680698e5d05ef526a53628c6bf413c4f33d25
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-25 13:29:09 +0900

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,9 @@ option = st.selectbox(
     "좋아하는 음식을 선택하세요:",
     ("망고빙수", "우유빙수", "복숭아빙수", "팥빙수"))
 
-st.button("선택초기화", type="primary")
+if st.button("선택초기화", type="primary"):
+    title=""
+    option=""
 if st.button("선택 후 눌러주세요"):
     st.write(title,"씨가 좋아하는 음식은",option,"입니다.")
 else:
