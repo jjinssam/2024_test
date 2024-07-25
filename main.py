8a496efeba9bbfadbbc4129f323f63b727f992b4
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-25 13:12:13 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,3 +2,8 @@ import streamlit as st
 
 st.write('Hello, *World!* :sunglasses:')
 st.title('간단한 인사말 생성기')
+
+st.write('이름을 입력하세요:')
+title = st.text_input(placeholder='한글이름')
+
+st.write('좋아하는 음식을 선택하세요:')
