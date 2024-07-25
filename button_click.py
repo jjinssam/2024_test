84737a9335346e57d36617559eadbbd193bd08f2
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-25 16:25:14 +0900

    Update button_click.py

--- button_click.py
+++ button_click.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-cnt = 0
+if "cnt" not in st.session_state: 
+  st.session_state.cnt=0
 if st.button('버튼버튼버튼!!!',key=0): 
-  cnt += 1
-st.write(f'### 누른 횟수는 :green[{cnt}] 입니다.')
+  st.session_state.cnt += 1
+st.write(f'### 누른 횟수는 :green[{st.session_state.cnt}] 입니다.')
