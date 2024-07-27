700c1950c1bcad5d75e7133bf8b7cd5f59a183ad
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-27 14:51:39 +0900

    Update bus01.py

--- bus01.py
+++ bus01.py
@@ -5,7 +5,6 @@ import pandas as pd
 # Function to fetch real-time bus data
 def get_bus_data():
     api_key ='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi%2FA6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq%2FJ3l6DHA3Q%3D%3D'
-#    api_key ='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi%2FA6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq%2FJ3l6DHA3Q%3D%3D'
 #    api_key='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi/A6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq/J3l6DHA3Q=='
     url = f'﻿http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?serviceKey={api_key}'
     st.write(url)
