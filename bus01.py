1cc98ee907a6c48438aa7d0b93551aa6e820f3e6
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-27 14:20:35 +0900

    Update bus01.py

--- bus01.py
+++ bus01.py
@@ -6,7 +6,7 @@ import pandas as pd
 def get_bus_data():
     api_key = '7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi%2FA6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq%2FJ3l6DHA3Q%3D%3D'
 #    api_key='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi/A6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq/J3l6DHA3Q=='
-    url = f'http://api_url_here?serviceKey={api_key}'
+    url = f'﻿http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?serviceKey={api_key}'
     st.write(url)
     response = requests.get(url)
     st.write(response) #추가
