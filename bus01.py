0c5cd204857774123f9f7f5679e275ed82fc9f06
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-29 14:17:05 +0900

    Update bus01.py

--- bus01.py
+++ bus01.py
@@ -7,7 +7,7 @@ api_key='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi/A6RrxDW7v2eh61RvZYTY29e1WFiL0u9
 # url = f'﻿http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt?ServiceKey={api_key}&busRouteId=100100118&startOrd=1&endOrd=13'
 #  url = f'﻿http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?serviceKey={api_key}'
 #test code
-url= 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?ServiceKey=api_key&busRouteId=100100016&startOrd=1&endOrd=13'
+url= 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt?ServiceKey=api_key&busRouteId=100100118&startOrd=1&endOrd=13'
 # params ={'serviceKey' : api_key, 'busRouteId' : '', 'startOrd' : '1', 'endOrd' : '10' }
 # response = requests.get(url, params=params)
 response = requests.get(url)
