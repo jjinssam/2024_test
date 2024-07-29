e1e0074f6667b6fbc4deab0c4da6d39b60de80aa
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-29 14:38:59 +0900

    Update bus02_test.py

--- bus02_test.py
+++ bus02_test.py
@@ -1,16 +1,14 @@
 import streamlit as st
 import requests
-import pandas as pd
 
-# api_key ='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi%2FA6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq%2FJ3l6DHA3Q%3D%3D'
-api_key='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi/A6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq/J3l6DHA3Q=='
-# url = f'﻿http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt?ServiceKey={api_key}&busRouteId=100100118&startOrd=1&endOrd=13'
-#  url = f'﻿http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?serviceKey={api_key}'
+api_key = '7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi/A6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq/J3l6DHA3Q=='
 
-#test code
-url= 'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt?ServiceKey=api_key&busRouteId=100100118&startOrd=1&endOrd=13'
-# params ={'serviceKey' : api_key, 'busRouteId' : '', 'startOrd' : '1', 'endOrd' : '10' }
-# response = requests.get(url, params=params)
+# 올바르게 API 키를 URL에 포함
+url = f'http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt?ServiceKey={api_key}&busRouteId=100100118&startOrd=1&endOrd=13'
 
 response = requests.get(url)
-st.write(response.content)
+
+if response.status_code == 200:
+    st.write(response.content)
+else:
+    st.error(f"API 요청 실패: {response.status_code}")
