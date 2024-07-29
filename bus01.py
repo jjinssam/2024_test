19f09daa00372885e59ad26466ec09005abb573c
jjinssam <125240819+jjinssam@users.noreply.github.com>
2024-07-29 13:30:35 +0900

    Update bus01.py

--- bus01.py
+++ bus01.py
@@ -2,8 +2,8 @@ import streamlit as st
 import requests
 import pandas as pd
 
-api_key ='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi%2FA6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq%2FJ3l6DHA3Q%3D%3D'
-# api_key='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi/A6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq/J3l6DHA3Q=='
+# api_key ='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi%2FA6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq%2FJ3l6DHA3Q%3D%3D'
+api_key='7LxzdtBw0bYtbx4A4BmxQRkAcrE1pFipyUFEyMi/A6RrxDW7v2eh61RvZYTY29e1WFiL0u9xtucq/J3l6DHA3Q=='
 # url = f'﻿http://ws.bus.go.kr/api/rest/buspos/getBusPosByRouteSt?ServiceKey={api_key}&busRouteId=100100118&startOrd=1&endOrd=13'
 #  url = f'﻿http://ws.bus.go.kr/api/rest/buspos/getBusPosByRtid?serviceKey={api_key}'
 #test code
